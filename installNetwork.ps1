4232de6240ee200d155952519104519c41bb8677
StudyWithMe9x <remoteathena@gmail.com>
2025-03-27 06:02:48 -0700

    Update installNetwork.ps1

--- installNetwork.ps1
+++ installNetwork.ps1
@@ -1,2 +1,2 @@
 <# 1. download and install net lookout admin#>
-$Path = $env:TEMP -WorkingDirectory; $Installer = "network.exe"; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://github.com/StudyWithMe9x/NetworkSoftware/blob/main/Setup_x64.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/y /silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer 
+$Path = $env:TEMP; $Installer = "network.exe"; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://github.com/StudyWithMe9x/NetworkSoftware/blob/main/Setup_x64.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/y /silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer 
